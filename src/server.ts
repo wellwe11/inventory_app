db2e40a8ffc795b828f6c79ad48d5572a87f149a
Robin Ryan <robin1ryan@hotmail.com>
2026-04-14 09:03:35 +0300

    implemeneted css

--- src/server.ts
+++ src/server.ts
@@ -69,6 +69,7 @@ const PORT = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "Views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(express.urlencoded({ extended: true }));
 
