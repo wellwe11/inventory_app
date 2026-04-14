88f96e4675270a0c32b12d5172a82b6d5d2bd4aa
Robin Ryan <robin1ryan@hotmail.com>
2026-04-14 09:22:59 +0300

    fix(filterForm): Minor adjustments

--- src/server.ts
+++ src/server.ts
@@ -69,8 +69,8 @@ const PORT = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "Views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "../public")));
 
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRoute);
