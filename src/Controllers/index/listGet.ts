12b4647b4b056cf701ba65138db2bab6a2c4f5d2
Robin Ryan <robin1ryan@hotmail.com>
2026-04-14 09:56:47 +0300

    added genre-filter

--- src/Controllers/index/listGet.ts
+++ src/Controllers/index/listGet.ts
@@ -1,7 +1,22 @@
 import type { Request, Response } from "express";
 
+const genres = [
+  "Action",
+  "Comedy",
+  "Drama",
+  "Horror",
+  "Sci-Fi",
+  "Romance",
+  "Thriller",
+  "Fantasy",
+  "Documentary",
+  "Animation",
+];
+
 const listGet = async (req: Request, res: Response) => {
-  res.render("index");
+  res.render("index", {
+    genres,
+  });
 };
 
 export default listGet;
