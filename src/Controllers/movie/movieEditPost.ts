c5f317d623c45e227168ae854bf849ba1b92c381
Robin Ryan <robin1ryan@hotmail.com>
2026-05-04 10:20:43 +0300

    General updates. Sanitizing user input, adding a POST to server that will update movie etc.

--- src/Controllers/movie/movieEditPost.ts
+++ src/Controllers/movie/movieEditPost.ts
@@ -1,13 +1,43 @@
-const movieEditPost = (req, res) => {
-  // Here we will first sanitise user-input
-  // Then first see if we find a matching director
-  // if not, create a new director
-  // add director.id to movie's director_id
-  // update genres if changed
-  // update movie
-  // redirect to movie?id=movie.id
+import { body, matchedData, validationResult } from "express-validator";
 
-  console.log("hi");
-};
+// for tomorrow: fix the POST form inside of movie.ejs.
+// It should link to id.
+// Also fix all inputs etc.
+// Submit and inputs should only be enabled if user has clicked edit.
+
+const alphaErr = "Temp err";
+const lengthErr = "Must contain at least one character.";
+
+// Update to check input even further
+const validateMovie = [
+  body("title")
+    .trim()
+
+    .isLength({ min: 1 })
+    .withMessage(lengthErr),
+];
+
+// Here we will first sanitise user-input
+// Then first see if we find a matching director
+// if not, create a new director
+// add director.id to movie's director_id
+// update genres if changed
+// update movie
+// redirect to movie?id=movie.id
+const movieEditPost = [
+  ...validateMovie,
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return res.status(400).render("/movie", {
+        err: errors.array(),
+      });
+    }
+
+    const { title, year, director_name } = req.body;
+  },
+];
 
 export default movieEditPost;
--- src/Controllers/movie/movieGet.ts
+++ src/Controllers/movie/movieGet.ts
@@ -2,11 +2,6 @@ import type { Request, Response } from "express";
 import { getMovie } from "../../DB/Queries/getMovie.js";
 import { getAllGenres } from "../../DB/Queries/getGenres.js";
 
-// for tomorrow: fix the POST form inside of movie.ejs.
-// It should link to id.
-// Also fix all inputs etc.
-// Submit and inputs should only be enabled if user has clicked edit.
-
 const movieGet = async (req: Request, res: Response) => {
   const movieId = req.query.id;
   const editMode = req.query.edit;
@@ -19,10 +14,12 @@ const movieGet = async (req: Request, res: Response) => {
 
   res.render("movie", {
     id,
-    title,
     src,
+
+    title,
     year,
     director_name,
+
     director_id,
     genre_list,
     allGenres: allGenres.map(({ name }) => name),
