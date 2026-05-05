63d576d40c940e9ce9099459e6439ef617b6a526
Robin Ryan <robin1ryan@hotmail.com>
2026-05-05 09:29:53 +0300

    feat(movie.ejs): Updated so genres are interactive only in edit-mode

--- src/DB/Queries/updateMovie.ts
+++ src/DB/Queries/updateMovie.ts
@@ -2,14 +2,6 @@ import pool from "../pool/pool.js";
 import { getOrAltarDirectors } from "./getOrAltarDirectors.js";
 
 export const updateMovie = async (id, title, year, director_name, genres) => {
-  // check if director has changed
-  // If it has, update directors.
-  // Find matching directors name.
-  // If found:
-  //   return director_id & add it to the update-movies query
-  // If not found:
-  //   return the new id and add it to the query
-
   const directorRows = await getOrAltarDirectors(director_name);
 
   const { rows } = await pool.query(
