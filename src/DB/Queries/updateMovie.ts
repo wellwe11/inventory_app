57a0b344ee5c6fd875644f1a9203e28928cadf17
Robin Ryan <robin1ryan@hotmail.com>
2026-05-12 10:43:11 +0300

    change(updateMovie): now takes src as argument as well to update image. This is done so I can add a src if it does not exist. I will also allow user to edit image in the future on editMovie

--- src/DB/Queries/updateMovie.ts
+++ src/DB/Queries/updateMovie.ts
@@ -1,14 +1,14 @@
 import pool from "../pool/pool.js";
 
-export const updateMovie = async (id, title, year, director_id) => {
+export const updateMovie = async (id, title, year, director_id, src) => {
   const { rows } = await pool.query(
     `
         UPDATE movies 
-        SET title = ($1), year = ($2), director_id = ($3)
-        WHERE id = ($4)
+        SET title = ($1), year = ($2), director_id = ($3), src = ($4)
+        WHERE id = ($5)
         RETURNING *;
         `,
-    [title, year, director_id, id],
+    [title, year, director_id, src, id],
   );
 
   return rows;
